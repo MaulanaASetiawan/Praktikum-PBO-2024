55dee421bffe0917734050e9dd9234e4a4b58b34
MaulanaASetiawan <maulanaa.agss@gmail.com>
2024-03-24 11:23:06 +0800

    Posttest-3

---
+++ Pbo_3/App/destinyclash.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2024 at 04:20 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `destinyclash`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enemy`
+--
+
+CREATE TABLE `enemy` (
+  `id_enemy` int(11) NOT NULL,
+  `enemy_name` varchar(50) NOT NULL,
+  `health` int(100) NOT NULL,
+  `damage` int(100) NOT NULL,
+  `xp` int(100) NOT NULL,
+  `stage` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `enemy`
+--
+
+INSERT INTO `enemy` (`id_enemy`, `enemy_name`, `health`, `damage`, `xp`, `stage`) VALUES
+(1, 'Cruz', 25, 5, 10, 1),
+(2, 'Brock', 50, 15, 15, 2),
+(3, 'Quad', 60, 20, 25, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `id_player` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `player_name` varchar(50) NOT NULL,
+  `health` int(100) NOT NULL,
+  `damage` int(100) NOT NULL,
+  `xp` int(100) NOT NULL,
+  `level` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `player`
+--
+
+INSERT INTO `player` (`id_player`, `id_user`, `player_name`, `health`, `damage`, `xp`, `level`) VALUES
+(1, 1, 'Bibim', 100, 10, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `Password`) VALUES
+(1, 'user1', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `enemy`
+--
+ALTER TABLE `enemy`
+  ADD PRIMARY KEY (`id_enemy`);
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`id_player`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `enemy`
+--
+ALTER TABLE `enemy`
+  MODIFY `id_enemy` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `player`
+--
+ALTER TABLE `player`
+  MODIFY `id_player` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `player`
+--
+ALTER TABLE `player`
+  ADD CONSTRAINT `player_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Pbo_3/App/src/Enemy.java
@@ -0,0 +1,322 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Enemy extends Chara{
+    private int stage;
+
+    public Enemy(String name, int health, int damage,int xp, int stage) {
+        super(name, health, damage, xp);
+        this.stage = stage;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public void display() {
+        System.out.println("+-------------------+");
+        System.out.println("Stage: " + stage);
+        System.out.println("Name: " + name);
+        System.out.println("Health: " + health);
+        System.out.println("Damage: " + damage);
+        System.out.println("Exp given: " + xp);
+        System.out.println("+-------------------+");
+        
+    }
+}
+
+class EnemyManager {
+    private static Connection conn;
+    private static PreparedStatement pstmt;
+    public static void connect() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/destinyclash", "root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void addEnemy() throws IOException, SQLException {
+        Enemy enemy = new Enemy(null, 0, 0, 0, 0);
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Stage: ");
+        int stage = inputCheck(input.readLine());
+        System.out.print("Enter Enemy name: ");
+        String name = input.readLine();
+        System.out.print("Enter Enemy health: ");
+        int health = inputCheck(input.readLine());
+        System.out.print("Enter Enemy damage: ");
+        int damage = inputCheck(input.readLine());
+        System.out.print("Enter Exp given: ");
+        int xp = inputCheck(input.readLine());
+
+        enemy.setStage(stage);
+        enemy.setName(name);
+        enemy.setHealth(health);
+        enemy.setDamage(damage);
+        enemy.setXp(xp);
+
+        saveEnemy(enemy);
+    }
+
+    public static void saveEnemy(Enemy enemy) throws SQLException, IOException {
+        connect();
+        String sql = "INSERT INTO enemy (stage, enemy_name, health, damage, xp) VALUES (?, ?, ?, ?, ?)";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, enemy.getStage());
+        pstmt.setString(2, enemy.getName());
+        pstmt.setInt(3, enemy.getHealth());
+        pstmt.setInt(4, enemy.getDamage());
+        pstmt.setInt(5, enemy.getXp());
+        pstmt.executeUpdate();
+
+        System.out.println();
+        System.out.println("Enemy added successfully!");
+        hold(2);
+        menuAdmin();
+    }
+    
+    public static void displayAllEnemies() throws SQLException, IOException {
+        connect();
+
+        if(countEnemies() == 0){
+            System.out.println("No enemies to display!");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+
+        String sql = "SELECT * FROM enemy";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                int id = rs.getInt("id_enemy");
+                int stage = rs.getInt("stage");
+                String name = rs.getString("enemy_name");
+                int health = rs.getInt("health");
+                int damage = rs.getInt("damage");
+                int xp = rs.getInt("xp");
+                Enemy enemy = new Enemy(name, health, damage, xp, stage);
+                System.out.println("ID: " + id);
+                enemy.display();
+            }
+        }
+    }
+
+    public static void updateEnemy() throws SQLException, IOException {
+        connect();
+        displayAllEnemies();
+    
+        if (countEnemies() == 0) {
+            System.out.println("No enemies to update!");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+    
+        int id = 0;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the ID of the enemy you want to update: ");
+        try {
+            id = Integer.parseInt(input.readLine());
+            if (id == 0) {
+                menuAdmin();
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid choice! Please enter a valid number.");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+
+        if (!isEnemyExists(id)) {
+            System.out.println("Enemy with ID " + id + " does not exist!");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+    
+        Enemy enemy = new Enemy(null, 0, 0, 0, 0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the new name for the enemy: ");
+        String newName = br.readLine();
+        System.out.print("Enter the new health for the enemy: ");
+        int newHealth = inputCheck(br.readLine());
+        System.out.print("Enter the new damage for the enemy: ");
+        int newDamage = inputCheck(br.readLine());
+        System.out.print("Enter the new xp given: ");
+        int newXp = inputCheck(br.readLine());
+    
+        enemy.setName(newName);
+        enemy.setHealth(newHealth);
+        enemy.setDamage(newDamage);
+        enemy.setXp(newXp);
+    
+        String sql = "UPDATE enemy SET enemy_name = ?, health = ?, damage = ?, xp = ? WHERE id_enemy = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, enemy.getName());
+            pstmt.setInt(2, enemy.getHealth());
+            pstmt.setInt(3, enemy.getDamage());
+            pstmt.setInt(4, enemy.getXp());
+            pstmt.setInt(5, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error updating enemy: " + e.getMessage());
+            hold(1);
+            menuAdmin();
+            return;
+        }
+
+        System.out.println();
+        System.out.println("Enemy updated successfully!");
+        hold(1);
+        menuAdmin();
+    }
+    
+
+    public static void deleteEnemy() throws SQLException, IOException {
+        connect();
+        displayAllEnemies();
+
+        if(countEnemies() == 0){
+            System.out.println("No enemies to delete!");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+
+        int id = 0;
+        int choice;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the ID of the enemy you want to delete: ");
+        try {
+            choice = Integer.parseInt(input.readLine());
+            if(choice == 0){
+                menuAdmin();
+                return;
+            }else{
+                id = choice;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid choice! Please enter a valid number.");
+            menuAdmin();
+            return;
+        }
+
+        String sql = "DELETE FROM enemy WHERE id_enemy = '"+id+"'";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.executeUpdate();
+
+        System.out.println();
+        System.out.println("Enemy deleted successfully!");
+        hold(1);
+        menuAdmin();
+    }
+
+    public static void menuAdmin() throws IOException, SQLException{
+        clear();
+        int choice;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("+-------------------+");
+        System.out.println("|  [1] Add Enemy    |");
+        System.out.println("|  [2] View Enemy   |");
+        System.out.println("|  [3] Update Enemy |");
+        System.out.println("|  [4] Delete Enemy |");
+        System.out.println("|  [0] Logout       |");
+        System.out.println("+-------------------+");
+        System.out.print("Enter your choice _> ");
+        try {
+            choice = Integer.parseInt(input.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid choice! Please enter a valid number.");
+            hold(1);
+            menuAdmin();
+            return;
+        }
+        switch(choice){
+            case 1 -> addEnemy();
+            case 2 -> {
+                displayAllEnemies();
+                System.out.println("Press Enter to go back to Admin menu...");
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                br.readLine();
+                menuAdmin();
+            }
+            case 3 -> updateEnemy();
+            case 4 -> deleteEnemy();
+            case 0 -> System.exit(0);
+            default -> {
+                System.out.println("Invalid choice! Please enter a valid number.");
+                hold(1);
+                menuAdmin();
+            }
+        }
+
+    }
+
+    private static void hold(int time){
+        try {
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void clear(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static int inputCheck(String input) throws IOException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input! Please enter a valid number.");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            return inputCheck(br.readLine());
+        }
+    }
+
+    public static int countEnemies() throws SQLException {
+        connect();
+        String sql = "SELECT COUNT(*) FROM enemy";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
+        return 0;
+    }
+
+    private static boolean isEnemyExists(int id) throws SQLException {
+        String sql = "SELECT * FROM enemy WHERE id_enemy = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rs.next();
+            }
+        }
+    }
+}
